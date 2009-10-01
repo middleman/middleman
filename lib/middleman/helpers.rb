087f48c98dba673da1e6518547a246840d484718
tdreyno <tdreyno@gmail.com>
2009-10-01 11:44:21 -0700

    better helper logic

--- lib/middleman/helpers.rb
+++ lib/middleman/helpers.rb
@@ -25,22 +25,22 @@ module Middleman
       path.include?("://") ? path : File.join(base_url, path)
     end
 
-    def image_tag(path, options={})
-      options[:alt] ||= ""
-      params = options.merge(:src => asset_url(path, self.images_dir))
+    def image_tag(path, params={})
+      params[:alt] ||= ""
+      params = params.merge(:src => asset_url(path, options.images_dir))
       params = params.map { |k,v| %Q{#{k}="#{v}"}}.join(' ')
       "<img #{params} />"
     end
 
-    def javascript_include_tag(path, options={})
-      params = options.merge(:src => asset_url(path, self.js_dir), :type => "text/javascript")
+    def javascript_include_tag(path, params={})
+      params = params.merge(:src => asset_url(path, options.js_dir), :type => "text/javascript")
       params = params.map { |k,v| %Q{#{k}="#{v}"}}.join(' ')
       "<script #{params}></script>"
     end
 
-    def stylesheet_link_tag(path, options={})
-      options[:rel] ||= "stylesheet"
-      params = options.merge(:href => asset_url(path, self.css_dir), :type => "text/css")
+    def stylesheet_link_tag(path, params={})
+      params[:rel] ||= "stylesheet"
+      params = params.merge(:href => asset_url(path, options.css_dir), :type => "text/css")
       params = params.map { |k,v| %Q{#{k}="#{v}"}}.join(' ')
       "<link #{params} />"
     end
