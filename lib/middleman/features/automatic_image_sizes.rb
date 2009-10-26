8fcb1d01334e02e733d6e229243fa52a6f50141f
tdreyno <tdreyno@gmail.com>
2009-10-26 15:08:21 -0700

    automatic image size calculations

--- lib/middleman/features/automatic_image_sizes.rb
+++ lib/middleman/features/automatic_image_sizes.rb
@@ -3,11 +3,11 @@ require "middleman/fastimage"
 class Middleman::Base
   alias_method :pre_automatic_image_tag, :image_tag
   helpers do
-    def image_tag(path, params={})
-      params[:alt] ||= ""
-      prefix = options.http_images_path rescue options.images_dir
-      
+    def image_tag(path, params={})      
       if (!params[:width] || !params[:height]) && !path.include?("://")
+        params[:alt] ||= ""
+        prefix = options.http_images_path rescue options.images_dir
+        
         begin
           real_path = File.join(options.public, asset_url(path, prefix))
           if File.exists? real_path
@@ -17,11 +17,13 @@ class Middleman::Base
           end
         rescue
         end
+
+        params = params.merge(:src => asset_url(path, prefix))
+        params = params.map { |k,v| %Q{#{k}="#{v}"}}.join(' ')
+        "<img #{params} />"
+      else
+        pre_automatic_image_tag(path, params)
       end
-      
-      params = params.merge(:src => asset_url(path, prefix))
-      params = params.map { |k,v| %Q{#{k}="#{v}"}}.join(' ')
-      "<img #{params} />"
     end
   end
 end
