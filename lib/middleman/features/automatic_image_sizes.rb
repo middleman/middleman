de6a4679a64ebe194956dbac3f2625276333c37b
tdreyno <tdreyno@gmail.com>
2009-10-28 14:54:25 -0700

    fix auto image sizes

--- lib/middleman/features/automatic_image_sizes.rb
+++ lib/middleman/features/automatic_image_sizes.rb
@@ -3,13 +3,13 @@ require "middleman/fastimage"
 class Middleman::Base
   alias_method :pre_automatic_image_tag, :image_tag
   helpers do
-    def image_tag(path, params={})      
+    def image_tag(path, params={})
       if (!params[:width] || !params[:height]) && !path.include?("://")
         params[:alt] ||= ""
-        prefix = options.http_images_path rescue options.images_dir
+        http_prefix = options.http_images_path rescue options.images_dir
         
         begin
-          real_path = File.join(options.public, asset_url(path, prefix))
+          real_path = File.join(options.public, options.images_dir, path)
           if File.exists? real_path
             dimensions = Middleman::FastImage.size(real_path, :raise_on_failure => true)
             params[:width]  ||= dimensions[0]
@@ -18,7 +18,7 @@ class Middleman::Base
         rescue
         end
 
-        params = params.merge(:src => asset_url(path, prefix))
+        params = params.merge(:src => asset_url(path, http_prefix))
         params = params.map { |k,v| %Q{#{k}="#{v}"}}.join(' ')
         "<img #{params} />"
       else
