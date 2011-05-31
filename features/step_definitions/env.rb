757ff53062662e7c1759c17c9be60c61e7a10b22
Matthias Döring <matt@foryourcontent.de>
2011-06-01 01:36:32 +0200

    adding before hook

--- features/step_definitions/env.rb
+++ features/step_definitions/env.rb
@@ -1,3 +1,9 @@
 ENV["MM_DIR"] = File.join(File.dirname(File.dirname(File.dirname(__FILE__))), "fixtures", "test-app")
 require File.join(File.dirname(File.dirname(File.dirname(__FILE__))), 'lib', 'middleman')
 require "rack/test"
+
+# absolute views path
+# otherwise resolve_template (padrino-core) can't find templates
+Before do
+  Middleman::Server.views = File.join(Middleman::Server.root, "source")
+end
