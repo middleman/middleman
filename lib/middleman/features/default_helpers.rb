fa84cb2888211075e9ac8d660dcac993b07c6fd9
David Siegel <djsiegel@gmail.com>
2011-06-22 14:53:50 -0700

    Make `auto_link_tag` (formerly `auto_stylesheet_link_tag`) less brittle by handling paths ending in `.../directory`, `.../directory/`, and `.../directory/index.html` uniformly.

--- lib/middleman/features/default_helpers.rb
+++ lib/middleman/features/default_helpers.rb
@@ -19,27 +19,34 @@ module Middleman::Features::DefaultHelpers
       end
     end
 
-    def auto_tag(asset_ext, separator="/", asset_dir = '')
-      path = request.path_info.dup
-      path << self.class.index_file if path.match(%r{/$})
-      path = path.gsub(%r{^/}, '')
-      path = path.gsub(File.extname(path), '')
-      path = path.gsub("/", separator)
-      path << ".#{asset_ext}"
-
-      parent_dir =
-        case asset_ext
-        when :js  then self.class.js_dir
-        when :css then self.class.css_dir
-        else asset_dir
-        end
-      
-      public_file = File.join(self.class.public, parent_dir, path)
-      views = File.join(self.class.views, parent_dir, "#{path}.*")
-
-      if File.exist?(public_file) || Dir[views].any?
-        yield path
+    def auto_link_tag(asset_ext, separator="/", asset_dir=nil)
+      def find_public_or_view_files(path)
+        files = Dir[File.join(self.class.views, "#{path}.*")]
+        public_file = File.join(self.class.public, path)
+        files << public_file if File.exists?(public_file)
+        files
       end
+      def find_public_or_view_directories(path)
+        [self.class.views, self.class.public].map { |d| File.join(d, path) }.select { |f| Dir.exist?(f) }
+      end
+
+      if asset_dir.nil?
+        asset_dir = case asset_ext
+          when :js  then self.class.js_dir
+          when :css then self.class.css_dir
+        end
+      end
+
+      path = request.path_info.dup
+      # Remove leading and trailing slashes.
+      path = path.gsub(%r{^/}, '').gsub(%r{/$}, '')
+      # If path is a directory, append index_file.
+      path = File.join(path, self.class.index_file) if find_public_or_view_directories(path).any?
+      # Replace the path extension with the asset extension.
+      path = path.gsub(File.extname(path), ".#{asset_ext}")
+      path = path.gsub("/", separator)
+
+      yield path if find_public_or_view_files(File.join asset_dir, path).any?
     end
 
     def page_classes
