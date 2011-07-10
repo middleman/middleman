8fa78ce8c9658245ae73bee376687c7c230d7d05
Thomas Reynolds <tdreyno@gmail.com>
2011-07-10 13:36:56 -0700

    further simplify coffee renderer

--- lib/middleman/renderers/coffee_script.rb
+++ lib/middleman/renderers/coffee_script.rb
@@ -1,12 +1,8 @@
-module Middleman
-  module Renderers
-    module CoffeeScript
-      class << self
-        def registered(app)
-          require "coffee_script"
-        end
-        alias :included :registered
-      end
+module Middleman::Renderers::CoffeeScript
+  class << self
+    def registered(app)
+      require "coffee_script"
     end
+    alias :included :registered
   end
 end
