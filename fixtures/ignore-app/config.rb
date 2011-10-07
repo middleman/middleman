545d573c7b0241d08c956eeb0d13054638167556
Thomas Reynolds <me@tdreyno.com>
2011-10-07 12:40:59 -0700

    test ignore and proxy

---
+++ fixtures/ignore-app/config.rb
@@ -0,0 +1,2 @@
+page "/my_article.html", :proxy => "/template.html", :ignore => true, :layout => false
+page "/articles/my_article.html", :proxy => "/articles/template.html", :ignore => true, :layout => false
