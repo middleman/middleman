79ded54e9cc031a07410e86574eaccd7571e6240
Thomas Reynolds <me@tdreyno.com>
2011-12-21 13:23:43 -0800

    fix cli docs

--- lib/middleman/cli.rb
+++ lib/middleman/cli.rb
@@ -5,21 +5,27 @@ require "thor/group"
 module Middleman::Cli
   
   class Base < Thor
-    include Thor::Actions
-
-    class_option "help", 
-      :type    => :boolean, 
-      :default => false, 
-      :aliases => "-h"
-    def initialize(*)
-      super
-      help_check if options[:help]
-    end
-
     desc "version", "Show version"
     def version
       require 'middleman/version'
       say "Middleman #{Middleman::VERSION}"
+    end
+    
+    def help(meth = nil, subcommand = false)
+      if meth && !self.respond_to?(meth)
+        klass, task = Thor::Util.find_class_and_task_by_namespace("#{meth}:#{meth}")
+        klass.start(["-h", task].compact, :shell => self.shell)
+      else
+        list = []
+        Thor::Util.thor_classes_in(Middleman::Cli).each do |klass|
+          list += klass.printable_tasks(false)
+        end
+        list.sort!{ |a,b| a[0] <=> b[0] }
+        
+        shell.say "Tasks:"
+        shell.print_table(list, :ident => 2, :truncate => true)
+        shell.say
+      end
     end
     
     def method_missing(meth, *args)
@@ -29,17 +35,9 @@ module Middleman::Cli
         meth = self.class.map[meth]
       end
       
-      # initialize_thorfiles(meth)
       klass, task = Thor::Util.find_class_and_task_by_namespace("#{meth}:#{meth}")
       args.unshift(task) if task
       klass.start(args, :shell => self.shell)
-    end
-    
-  private
-
-    def help_check
-      help self.class.send(:retrieve_task_name, ARGV.dup)
-      exit 0
     end
   end
 end
