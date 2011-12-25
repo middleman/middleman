cf6661fa4537518fde70a7de431d5d5af2813a34
Eric Skogen <eric.skogen@space150.com>
2011-12-25 01:02:50 -0600

    new page features useful for sitemap traversal support indexes which use the `directory_index` extension add a `data` accessor to pages

--- fixtures/traversal-app/config.rb
+++ fixtures/traversal-app/config.rb
@@ -1,2 +1,7 @@
+activate :directory_indexes
+
 page "/sub/fake.html", :proxy => "/proxied.html", :ignore => true
 page "/sub/fake2.html", :proxy => "/proxied.html", :ignore => true
+
+page "/directory-indexed/fake.html", :proxy => "/proxied.html", :ignore => true
+page "/directory-indexed/fake2.html", :proxy => "/proxied.html", :ignore => true
