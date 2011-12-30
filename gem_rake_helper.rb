4422efc270197ca77ff0100edf53e7af6972f05a
Thomas Reynolds <me@tdreyno.com>
2011-12-29 19:57:02 -0800

    fix release

--- gem_rake_helper.rb
+++ gem_rake_helper.rb
@@ -1,21 +1,23 @@
 require 'rubygems' unless defined?(Gem)
 require 'rake'
-
+require 'cucumber/rake/task'
+require 'yard'
 require 'bundler/gem_tasks'
 
 # Skip the releasing tag
 class Bundler::GemHelper
   def release_gem
     guard_clean
-    guard_already_tagged
+    # guard_already_tagged
     built_gem_path = build_gem
     rubygem_push(built_gem_path)
   end
 end
 
-require 'cucumber/rake/task'
 Cucumber::Rake::Task.new(:test, 'Run features that should pass') do |t|
   t.cucumber_opts = "--color --tags ~@wip --strict --format #{ENV['CUCUMBER_FORMAT'] || 'pretty'}"
 end
+
+YARD::Rake::YardocTask.new
 
 task :default => :test
