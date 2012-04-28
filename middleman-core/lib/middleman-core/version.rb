81e3d894c242d50bf63fc5eab773ea6c48f202b7
Thomas Reynolds <me@tdreyno.com>
2012-04-28 10:48:08 -0700

    Bump version [no-ci]

--- middleman-core/lib/middleman-core/version.rb
+++ middleman-core/lib/middleman-core/version.rb
@@ -4,7 +4,7 @@ require "rubygems"
 module Middleman
   # Current Version
   # @return [String]
-  VERSION = '3.0.0.beta.2' unless const_defined?(:VERSION)
+  VERSION = '3.0.0.beta.3' unless const_defined?(:VERSION)
   
   # Parsed version for RubyGems
   # @private
