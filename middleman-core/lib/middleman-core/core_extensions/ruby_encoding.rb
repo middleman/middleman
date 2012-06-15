96c29181aadb39ba92d039d9b375c7ac55ccf5bb
Rico Sta. Cruz <rico@ambiescent.com>
2012-06-16 06:07:44 +0800

    Implement the :encoding setting which defaults to 'utf-8'

---
+++ middleman-core/lib/middleman-core/core_extensions/ruby_encoding.rb
@@ -0,0 +1,24 @@
+# Simple extension to manage Ruby encodings
+module Middleman::CoreExtensions::RubyEncoding
+
+  # Setup extension
+  class << self
+    
+    # Once registerd
+    def registered(app)
+      app.send :include, InstanceMethods
+    end
+
+  end
+
+  module InstanceMethods
+    def initialize
+      if Object.const_defined?(:Encoding)
+        Encoding.default_internal = encoding
+        Encoding.default_external = encoding
+      end
+
+      super
+    end
+  end
+end
