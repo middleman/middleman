4629b52e789819b3567d0245a107d17ac221a605
Daniel Palmer <danny@dannyprose.com>
2012-08-08 20:11:38 -0700

    Added smaccs in properly

---
+++ middleman-more/lib/middleman-more/templates/smacss/README.md
@@ -0,0 +1,19 @@
+# SMACSS type starter template for Middleman
+
+If you use [Middleman](http://middlemanapp.com/), you can download this fileset to use as a starting point for new projects. This is inspired by Jonathan Snook's [SMACSS](https://smacss.com/) ideas, workshop and book.
+
+## How to use
+
+1. Download files and copy smacss into 
+```
+~/.middleman/ 
+```
+(you'll need to create the folder .middleman folder if it doesn't already exist).
+2. Initialize a new middleman project with the template flag 
+```
+middleman init my_new_smacss_project --template=smacss
+```
+
+You can read more about project templates on the [Middleman site](http://middlemanapp.com/getting-started/welcome/).
+
+ 
---
+++ middleman-more/lib/middleman-more/templates/smacss/config.rb
@@ -0,0 +1,77 @@
+### 
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install compass-susy-plugin
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+# 
+# With no layout
+# page "/path/to/file.html", :layout => false
+# 
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+# 
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'img'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+  
+  # Minify Javascript on build
+  # activate :minify_javascript
+  
+  # Enable cache buster
+  # activate :cache_buster
+  
+  # Use relative URLs
+  # activate :relative_assets
+  
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+  
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
