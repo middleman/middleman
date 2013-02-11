cf51fe6a20b5f2649d2bddbb616032fc5e8518d3
Ben Hollis <ben@benhollis.net>
2013-02-10 16:53:38 -0800

    Fix binary_spec test for svgz - it's binary

--- middleman-core/spec/middleman-core/binary_spec.rb
+++ middleman-core/spec/middleman-core/binary_spec.rb
@@ -1,13 +1,13 @@
 require 'middleman-core/util'
 
 describe "Middleman::Util#binary?" do
-  %w(plain.txt unicode.txt unicode stars.svgz).each do |file|
+  %w(plain.txt unicode.txt unicode).each do |file|
     it "recognizes #{file} as not binary" do
       Middleman::Util.binary?(File.join(File.dirname(__FILE__), "binary_spec/#{file}")).should be_false
     end
   end
 
-  %w(middleman.png middleman).each do |file|
+  %w(middleman.png middleman stars.svgz).each do |file|
     it "recognizes #{file} as binary" do
       Middleman::Util.binary?(File.join(File.dirname(__FILE__), "binary_spec/#{file}")).should be_true
     end
