96c44162463aa823d42b3f55577d7472eab0b4b0
Thomas Reynolds <me@tdreyno.com>
2014-08-15 09:41:19 -0700

    ver bump

--- middleman-core/lib/middleman-core/version.rb
+++ middleman-core/lib/middleman-core/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   # Current Version
   # @return [String]
-  VERSION = '4.0.0.pre.0' unless const_defined?(:VERSION)
+  VERSION = '4.0.0.alpha.1' unless const_defined?(:VERSION)
 end
