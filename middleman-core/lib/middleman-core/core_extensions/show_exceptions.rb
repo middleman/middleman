4626193f97ad3263846f9cb07f77b918f8a4c489
Thomas Reynolds <me@tdreyno.com>
2015-05-04 10:50:35 -0700

    Fix show_exceptions

--- middleman-core/lib/middleman-core/core_extensions/show_exceptions.rb
+++ middleman-core/lib/middleman-core/core_extensions/show_exceptions.rb
@@ -1,5 +1,6 @@
 require 'rack/showexceptions'
 
+
 # Support rack/showexceptions during development
 module Middleman::CoreExtensions
   class ShowExceptions < ::Middleman::Extension
@@ -8,7 +9,7 @@ module Middleman::CoreExtensions
 
       return if app.config.defines_setting? :show_exceptions
 
-      app.config.define_setting :show_exceptions, !!ENV['TEST'], 'Whether to catch and display exceptions'
+      app.config.define_setting :show_exceptions, ENV['TEST'] ? false : true, 'Whether to catch and display exceptions'
     end
 
     def after_configuration
