68a6eacc333b283bf90df1519a4b55660fa82de3
Mauro Otonelli <mauro.otonelli@gmail.com>
2015-07-15 22:03:41 -0300

    Removed most deprecation warnings from the build.

--- middleman-core/lib/middleman-core/step_definitions/server_steps.rb
+++ middleman-core/lib/middleman-core/step_definitions/server_steps.rb
@@ -31,8 +31,7 @@ Given /^current environment is "([^\"]*)"$/ do |env|
 end
 
 Given /^the Server is running$/ do
-  root_dir = File.expand_path(current_dir)
-
+  root_dir = File.expand_path(current_directory)
 
   if File.exists?(File.join(root_dir, 'source'))
     ENV['MM_SOURCE'] = 'source'
@@ -48,7 +47,7 @@ Given /^the Server is running$/ do
     set :show_exceptions, false
   }
 
-  in_current_dir do
+  in_current_directory do
     @server_inst = Middleman::Application.server.inst do
       initialize_commands.each do |p|
         instance_exec(&p)
@@ -69,62 +68,62 @@ Given /^a template named "([^\"]*)" with:$/ do |name, string|
 end
 
 When /^I go to "([^\"]*)"$/ do |url|
-  in_current_dir do
+  in_current_directory do
     visit(URI.encode(url).to_s)
   end
 end
 
 Then /^going to "([^\"]*)" should not raise an exception$/ do |url|
-  in_current_dir do
-    
+  in_current_directory do
+
     expect{ visit(URI.encode(url).to_s) }.to_not raise_exception
   end
 end
 
 Then /^the content type should be "([^\"]*)"$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.response_headers['Content-Type']).to start_with expected
   end
 end
 
 Then /^I should see "([^\"]*)"$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.body).to include expected
   end
 end
 
 Then /^I should see '([^\']*)'$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.body).to include expected
   end
 end
 
 Then /^I should see:$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.body).to include expected
   end
 end
 
 Then /^I should not see "([^\"]*)"$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.body).not_to include expected
   end
 end
 
 Then /^I should not see:$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.body).not_to include expected
   end
 end
 
 Then /^the status code should be "([^\"]*)"$/ do |expected|
-  in_current_dir do
+  in_current_directory do
     expect(page.status_code).to eq expected.to_i
   end
 end
 
 Then /^I should see "([^\"]*)" lines$/ do |lines|
-  in_current_dir do
+  in_current_directory do
     expect(page.body.chomp.split($/).length).to eq lines.to_i
   end
 end
