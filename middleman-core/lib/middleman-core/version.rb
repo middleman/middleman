40f023f01cb02e2972ac6732b70d518db68a1ae5
Thomas Reynolds <me@tdreyno.com>
2015-09-12 16:15:32 -0700

    Let's do this

--- middleman-core/lib/middleman-core/version.rb
+++ middleman-core/lib/middleman-core/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   # Current Version
   # @return [String]
-  VERSION = '4.0.0.beta.3' unless const_defined?(:VERSION)
+  VERSION = '4.0.0.rc.1' unless const_defined?(:VERSION)
 end
