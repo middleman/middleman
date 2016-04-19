2b0f720850ca80e9f28392112d80a47939582a93
Thomas Reynolds <me@tdreyno.com>
2016-04-19 11:54:00 -0700

    Bad merge

--- middleman-core/lib/middleman-core/template_context.rb
+++ middleman-core/lib/middleman-core/template_context.rb
@@ -34,18 +34,6 @@ module Middleman
       @app = app
       @locs = locs
       @opts = opts
-
-      @locs.each do |k, _|
-        next unless self.respond_to?(k)
-
-        msg = "Template local `#{k}` tried to overwrite an existing context value. Please renamed the key when passing to `locals`"
-
-        if app.build?
-          throw msg
-        else
-          logger.error(msg)
-        end
-      end
     end
 
     # Return the current buffer to the caller and clear the value internally.
