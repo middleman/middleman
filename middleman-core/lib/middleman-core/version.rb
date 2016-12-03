b6167b1369827d871421eb0ee88cb7bf15fbdc5c
Thomas Reynolds <me@tdreyno.com>
2016-12-03 11:32:09 -0800

    Fix #2019

--- middleman-core/lib/middleman-core/version.rb
+++ middleman-core/lib/middleman-core/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   # Current Version
   # @return [String]
-  VERSION = '4.1.13'.freeze unless const_defined?(:VERSION)
+  VERSION = '4.1.14'.freeze unless const_defined?(:VERSION)
 end
