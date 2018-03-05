b5fd32eabbcd1fb102a3a49da911bd80d423744b
Thomas Reynolds <me@tdreyno.com>
2018-03-05 15:35:43 -0800

    Prep version

--- middleman-core/lib/middleman-core/version.rb
+++ middleman-core/lib/middleman-core/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   # Current Version
   # @return [String]
-  VERSION = '4.2.1'.freeze unless const_defined?(:VERSION)
+  VERSION = '4.3.0.rc.1'.freeze unless const_defined?(:VERSION)
 end
