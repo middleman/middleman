2d6c7555e304190491c99cd379da56afb344c716
Thomas Reynolds <me@tdreyno.com>
2018-10-17 12:13:29 -0700

    Bump docker

--- middleman-docker/build.sh
+++ middleman-docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-MIDDLEMAN_VERSION=4.3.0.rc.2
+MIDDLEMAN_VERSION=4.3.0.rc.3
 
 docker build . -t middleman/middleman:"$MIDDLEMAN_VERSION" --build-arg MIDDLEMAN_VERSION="$MIDDLEMAN_VERSION"
