08db6f3f66eece7208f72a872f99f72b2b5348d1
Thomas Reynolds <me@tdreyno.com>
2018-11-08 11:15:16 -0800

    Update directory_indexes.rb

--- middleman-core/lib/middleman-core/extensions/directory_indexes.rb
+++ middleman-core/lib/middleman-core/extensions/directory_indexes.rb
@@ -7,12 +7,12 @@ class Middleman::Extensions::DirectoryIndexes < ::Middleman::Extension
   self.resource_list_manipulator_priority = 100
 
   EXTENSIONS = Set.new %w[.htm .html .php .xhtml]
- 
+
   Contract IsA['Middleman::Sitemap::ResourceListContainer'] => Any
   def manipulate_resource_list_container!(resource_list)
     index_file = app.config[:index_file]
     new_index_path = "/#{index_file}"
-    
+
     resource_list.by_extensions(EXTENSIONS).each do |resource|
       next if resource.destination_path == index_file ||
               resource.destination_path.end_with?(new_index_path)
