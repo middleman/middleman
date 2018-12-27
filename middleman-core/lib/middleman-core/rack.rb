3683cd342eda9b55d653a961727f5f3dde2948a2
Thomas Reynolds <me@tdreyno.com>
2018-12-27 15:07:22 -0800

    Fix mime check

--- middleman-core/lib/middleman-core/rack.rb
+++ middleman-core/lib/middleman-core/rack.rb
@@ -141,7 +141,10 @@ module Middleman
       response[1]['Content-Encoding'] = 'gzip' if %w[.svgz .gz].include?(resource.ext)
       # Do not set Content-Type if status is 1xx, 204, 205 or 304, otherwise
       # Rack will throw an error (500)
-      response[1]['Content-Type'] = resource.content_type || 'application/octet-stream' if !(100..199).cover?(status) && ![204, 205, 304].include?(status)
+      if !(100..199).cover?(status) && ![204, 205, 304].include?(status)
+        response[1]['Content-Type'] = resource.content_type || (resource.binary? ? 'application/octet-stream' : 'text/plain')
+      end
+
       halt response
     end
   end
