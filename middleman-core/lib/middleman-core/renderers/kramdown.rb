9155d02c1ddb2c59061c0b1a80d3d95afbb9d4db
Maarten de Waard <maarten@greenhost.nl>
2019-08-09 12:47:44 +0200

    Pass &:to_sym as an argument to transform_keys! instead of a block.

--- middleman-core/lib/middleman-core/renderers/kramdown.rb
+++ middleman-core/lib/middleman-core/renderers/kramdown.rb
@@ -47,7 +47,7 @@ module Middleman
 
         # options to link_to are expected to be symbols, but in Markdown
         # everything is a string.
-        attr.transform_keys! { | key | key.to_sym }
+        attr.transform_keys!(&:to_sym)
 
         scope.link_to(content, link, attr)
       end
