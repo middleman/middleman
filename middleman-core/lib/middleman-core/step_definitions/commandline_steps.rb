184b2b02d20d091fd8bf6226cba0c39c039f7a67
Akira Matsuda <ronnie@dio.jp>
2024-01-13 09:40:45 +0900

    set_env => set_environment_variable
    https://github.com/cucumber/aruba/commit/663991ec707cb8ee614daf0caffb78f832520508

--- middleman-core/lib/middleman-core/step_definitions/commandline_steps.rb
+++ middleman-core/lib/middleman-core/step_definitions/commandline_steps.rb
@@ -49,7 +49,7 @@ Given /I start a dns server with:/ do |string|
     )
   )
 
-  set_env 'PATH', File.expand_path(File.join(aruba.current_directory, 'bin')) + ':' + ENV['PATH']
+  set_environment_variable 'PATH', File.expand_path(File.join(aruba.current_directory, 'bin')) + ':' + ENV['PATH']
   write_file db_file, string
 
   @dns_server = run_command("dns_server.rb #{db_file} #{port}", exit_timeout: 120)
@@ -68,7 +68,7 @@ Given /I start a mdns server with:/ do |string|
     )
   )
 
-  set_env 'PATH', File.expand_path(File.join(aruba.current_directory, 'bin')) + ':' + ENV['PATH']
+  set_environment_variable 'PATH', File.expand_path(File.join(aruba.current_directory, 'bin')) + ':' + ENV['PATH']
   write_file db_file, string
 
   @mdns_server = run_command("dns_server.rb #{db_file} #{port}", exit_timeout: 120)
