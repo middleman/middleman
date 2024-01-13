94732573a0db765d60aa855eb499a40c61963ac4
Akira Matsuda <ronnie@dio.jp>
2024-01-13 09:37:03 +0900

    run => run_command
    https://github.com/cucumber/aruba/commit/4b94acc26c998bba5392ebd22be1b2da00fd0c2d

--- middleman-core/lib/middleman-core/step_definitions/commandline_steps.rb
+++ middleman-core/lib/middleman-core/step_definitions/commandline_steps.rb
@@ -22,7 +22,7 @@ end
 
 # Make it just a long running process
 Given /`(.*?)` is running in background/ do |cmd|
-  run(cmd, 120)
+  run_command(cmd, 120)
 end
 
 Given /I have a local hosts file with:/ do |string|
@@ -52,7 +52,7 @@ Given /I start a dns server with:/ do |string|
   set_env 'PATH', File.expand_path(File.join(current_dir, 'bin')) + ':' + ENV['PATH']
   write_file db_file, string
 
-  @dns_server = run("dns_server.rb #{db_file} #{port}", 120)
+  @dns_server = run_command("dns_server.rb #{db_file} #{port}", 120)
 end
 
 Given /I start a mdns server with:/ do |string|
@@ -71,7 +71,7 @@ Given /I start a mdns server with:/ do |string|
   set_env 'PATH', File.expand_path(File.join(current_dir, 'bin')) + ':' + ENV['PATH']
   write_file db_file, string
 
-  @mdns_server = run("dns_server.rb #{db_file} #{port}", 120)
+  @mdns_server = run_command("dns_server.rb #{db_file} #{port}", 120)
 end
 
 Given /I start a mdns server for the local hostname/ do
