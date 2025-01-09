0cb9caca6ec5222d4cf9710b5f1b11ae3977acf9
David Rodríguez <deivid.rodriguez@riseup.net>
2025-01-09 16:29:04 +0100

    Raise an error when rendering less templates
    Since support has been removed from tilt.

--- middleman-core/lib/middleman-core/renderers/less.rb
+++ middleman-core/lib/middleman-core/renderers/less.rb
@@ -1,36 +1,20 @@
-require 'less'
-
 module Middleman
   module Renderers
     # Sass renderer
     class Less < ::Middleman::Extension
-      define_setting :less, {}, 'LESS compiler options'
-
-      def initialize(app, options={}, &block)
-        super
-
-        # Tell Tilt to use it as well (for inline sass blocks)
-        ::Tilt.register 'less', LocalLoadingLessTemplate
-        ::Tilt.prefer(LocalLoadingLessTemplate)
-      end
-
-      def after_configuration
-        app.files.by_type(:source).watchers.each do |source|
-          ::Less.paths << (source.directory + app.config[:css_dir]).to_s
-        end
-      end
-
       # A SassTemplate for Tilt which outputs debug messages
-      class LocalLoadingLessTemplate < ::Tilt::LessTemplate
-        def prepare
-          if ::Less.const_defined? :Engine
-            @engine = ::Less::Engine.new(data)
-          else
-            parser = ::Less::Parser.new({}.merge!(options).merge!(filename: eval_file, line: line, paths: ['.', File.dirname(eval_file)]))
-            @engine = parser.parse(data)
-          end
+      class DummyLessTemplate < ::Tilt::Template
+        def evaluate(scope, locals, &block)
+          raise <<~ERROR
+            The builtin less renderer has been removed from middleman.
+            To continue using less, make sure to setup an external pipeline.
+            See external pipeline documentation at https://middlemanapp.com/advanced/external-pipeline
+            for more information.
+          ERROR
         end
       end
+
+      ::Tilt.register 'less', DummyLessTemplate
     end
   end
 end
