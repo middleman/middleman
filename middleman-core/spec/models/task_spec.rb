7cae28d1dc9ad652a19fe4855e3b05b5598cf7a2
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-08-22 15:05:36 +0200

    add tab characters

--- middleman-core/spec/models/task_spec.rb
+++ middleman-core/spec/models/task_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Task' do
   context 'some methods' do
     context '.do_work(value)' do
       context '  does work   ' do
-        it '   for some values           ' do
+        it ' \t \t \t  for some values           ' do
           expect(1).to eq(1)
         end
       end
